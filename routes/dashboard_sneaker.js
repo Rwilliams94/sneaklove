2f1fea9d6a98bdbe35a320d7265449f004646dbc
Magali <fort_magali@hotmail.fr>
2021-03-05 15:25:08 +0100

    add crud for sneakers on dashboard

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -10,23 +10,51 @@ const router = new express.Router(); // create an app sub-module (router)
 router.get("/", (req, res, next) => {
     SneakerModel.find()
     .then(sneakers => {
-        console.log(sneakers);
+        // console.log(sneakers);
         res.render("products_manage", {sneakers});
     })
     .catch(err=>console.error(err))    
 });
 
+// update sneaker
 router.get("/product-edit/:id", (req, res, next) => {
     SneakerModel.findById(req.params.id)
     .then(sneaker => {
-        console.log(sneaker.name)
+        // console.log(sneaker.name)
         res.render("product_edit", {sneaker})
     })
     .catch(err=>console.error(err))
 })
 
 router.post("/product-edit/:id", (req, res, next) => {
-    
+    SneakerModel.findByIdAndUpdate(req.params.id, req.body, {new:true})
+    .then(sneaker=> {
+        // console.log("req.params.id: ", req.params.id)
+        // console.log("sneaker:", sneaker);
+        // console.log("req.body: ",req.body);
+        res.redirect("/dashboard_sneaker");
+    })
+    .catch(err=>console.error(err))
+})
+
+//create a new sneaker
+router.get("/add", (req, res, next) => {
+    res.render("products_add")
+})
+
+router.post("/add", (req, res, next) => {
+    SneakerModel.create(req.body)
+    .then(sneaker=> {
+        res.redirect("/dashboard_sneaker");
+    })
+    .catch(err=>console.error(err))
+})
+
+//delete a sneaker
+router.get("/delete/:id", (req, res, next) => {
+    SneakerModel.findByIdAndDelete(req.params.id)
+    .then(res.redirect("/dashboard_sneaker"))
+    .catch(next)
 })
 
 module.exports = router;
